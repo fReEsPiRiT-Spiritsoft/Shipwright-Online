88da3dc16c3fa77e5e13d55a5af5193a370d5fa5
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2026-09-11 11:42:04 +0200

    fix: restructure OUTPUT_DIRECTORY configuration for clarity in CMake

--- soh/CMake/DefaultCXX.cmake
+++ soh/CMake/DefaultCXX.cmake
@@ -1,6 +1,10 @@
 include("${CMAKE_CURRENT_LIST_DIR}/Default.cmake")
 
-set_config_specific_property("OUTPUT_DIRECTORY" "${CMAKE_SOURCE_DIR}$<$<NOT:$<STREQUAL:${CMAKE_VS_PLATFORM_NAME},Win32>>:/${CMAKE_VS_PLATFORM_NAME}>/${PROPS_CONFIG}")
+if(CMAKE_GENERATOR MATCHES "Visual Studio|Xcode")
+    set_config_specific_property("OUTPUT_DIRECTORY" "${CMAKE_SOURCE_DIR}$<$<NOT:$<STREQUAL:${CMAKE_VS_PLATFORM_NAME},Win32>>:/${CMAKE_VS_PLATFORM_NAME}>/${PROPS_CONFIG}")
+else()
+    set_config_specific_property("OUTPUT_DIRECTORY" "${CMAKE_BINARY_DIR}")
+endif()
 
 if(MSVC)
     create_property_reader("DEFAULT_CXX_EXCEPTION_HANDLING")
